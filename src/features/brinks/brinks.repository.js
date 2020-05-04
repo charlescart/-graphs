5a8ac445c5b83a7f2e576873a0b602c12d3d1b63
Charles Rodriguez <rodriguezchat@gmail.com>
2020-05-04 10:11:11 -0300

    avance, agregado filtro de nodos imcumplidos

--- src/features/brinks/brinks.repository.js
+++ src/features/brinks/brinks.repository.js
@@ -1,7 +1,8 @@
-/* eslint-disable no-await-in-loop */
+/* eslint-disable no-continue */
 /* eslint-disable quotes */
-/* eslint-disable comma-dangle */
-const moment = require("moment");
+/* eslint-disable no-console */
+/* eslint-disable no-await-in-loop */
+const moment = require('moment');
 const {
   getFirstStrip,
   getDurationInTraffic,
@@ -13,6 +14,7 @@ const {
 const brinksRepository = {
   algorithm: async (req) => {
     let { nodes } = req.body;
+    const { hourDeparture } = req.body;
 
     // eslint-disable-next-line no-useless-catch
     // try {
@@ -20,105 +22,106 @@ const brinksRepository = {
     const currentDate = moment("00:00:00", "HH:mm:ss");
 
     /* hora de partida */
-    const currentTime = moment.duration(
-      moment("05:09:00", "HH:mm:ss").format("HH:mm:ss")
-    );
+    const currentTime = moment.duration(hourDeparture);
 
     /* tomo el node de orden cero, este siempre es el primero del array */
     let nodeRoot = nodes.shift();
+
+    /* nodos no cumplidos */
+    const unfulfilledNodes = [];
 
     /* ruta optima */
     // let route = [nodeRoot];
 
     do {
       for (let i = 0; i < nodes.length; i += 1) {
+        /* si el nodo está bloqueado no lo analizo */
+        if (nodes[i].blocked) continue;
+
         const node = nodes[i];
         // nodes.forEach(async node => {
-
-        /* si el nodo está bloqueado no lo analizo */
-        if (node.blocked) i += 1;
 
         /* calculo de cercania a la franja horaria */
         // let timeToNode = getTimeProximity(nodeRoot, node);
 
-        const numberBands = await getNumberTimeBandsAvailabilitys(
-          node,
-          currentTime
-        );
+        const numberBands = await getNumberTimeBandsAvailabilitys(node, currentTime);
 
         /* la franja horaria mas cercana con respecto a la hora actual */
 
         const firstStrip = await getFirstStrip(node, currentTime);
 
         /* Cercania */
-        const timeProximity = moment.duration(
-          moment.duration(firstStrip.start).subtract(currentTime)
-        );
+        const timeProximity = firstStrip.start.clone();
+        timeProximity.subtract(currentTime);
+        // moment.duration(moment.duration(firstStrip.start).subtract(currentTime));
 
         /* Ancho de franja horaria de atencion */
-        const timeBandWidth = moment.duration(
-          moment.duration(firstStrip.end) - moment.duration(firstStrip.start)
-        );
+        const timeBandWidth = firstStrip.end.clone();
+        timeBandWidth.subtract(firstStrip.start);
+        // moment.duration(moment.duration(firstStrip.end) - moment.duration(firstStrip.start));
 
         /* si el tiempo de servicio va dentro de la franja horaria */
-        if (node.serviceTimeWithin) {
-          timeBandWidth.subtract(moment.duration(node.serviceTime));
-        }
+        if (node.serviceTimeWithin) timeBandWidth.subtract(moment.duration(node.serviceTime));
 
         /* hora de llegada al nodo */
-        const durationInTraffic = await getDurationInTraffic(
-          nodeRoot,
-          node,
-          currentDate,
-          currentTime
-        );
+        const timeInTraffic = await getDurationInTraffic(nodeRoot, node, currentDate, currentTime);
 
         /* verifica si pase la franja horaria en el tiempo de llegada con trafico */
-        const expiredTimeSlot = await checkExpiredTimeSlot(
-          currentTime,
-          durationInTraffic,
-          firstStrip
-        );
+        const expiredTimeSlot = await checkExpiredTimeSlot(currentTime, timeInTraffic, firstStrip);
 
         /* llega dentro de la franja horaria? */
-        const arrival = await getArrival(
-          node,
-          currentTime,
-          durationInTraffic,
-          firstStrip
-        );
+        const arrival = await getArrival(node, currentTime, timeInTraffic, firstStrip);
 
-        node["analysis"] = {
+        node.analysis = {
           timeProximity,
           timeBandWidth,
-          durationInTraffic,
+          timeInTraffic,
           arrival,
-          hourArrival: moment.duration(currentTime + durationInTraffic),
+          hourArrival: moment.duration(currentTime + timeInTraffic),
           numberBands,
           expiredTimeSlot,
           // blockedNode: item que indique si el nodo se superpone a otro
         };
 
-        // eslint-disable-next-line no-console
-        console.log(
-          `${
-            node.description
-          }: departure: ${currentTime} durationInTraffic: ${durationInTraffic.asSeconds()} timeArrival: ${moment.duration(
-            currentTime + durationInTraffic
-          )}, Arrival?: ${arrival}, Cercania: ${timeProximity.asSeconds()}, anchodefranja: ${timeBandWidth.asSeconds()}, ${
-            firstStrip.start
-          } - ${
-            firstStrip.end
-          }, numberBands: ${numberBands}, expiredTimeSlot: ${expiredTimeSlot}`
-        );
+        console.log(`${node.description}: departure: ${currentTime} durationInTraffic: ${timeInTraffic.asSeconds()}
+          timeArrival: ${moment.duration(currentTime + timeInTraffic)}, Arrival?: ${arrival}, Cercania: ${timeProximity.asSeconds()},
+          anchodefranja: ${timeBandWidth.asSeconds()}, ${firstStrip.start} - ${firstStrip.end}, numberBands: ${numberBands},
+          expiredTimeSlot: ${expiredTimeSlot}`);
 
-        // eslint-disable-next-line no-console
         console.log(node);
       }
+
+      /* identificando a los nodos con los que no se podrá cumplir */
+      nodes.forEach((node, i, array) => {
+        /* solo nodos disponibles  sin superposicion */
+        if (!node.blocked) {
+          const { expiredTimeSlot, numberBands } = node.analysis;
+
+          /* se identifican los nodos caducados y se llevan a un array aparte */
+          if (expiredTimeSlot && numberBands <= 0) unfulfilledNodes.push(array.splice(i, 1));
+        }
+      });
+      /* fin de la identificacion de nodos no cumplidos */
+
+      /* identificando el nodo mas urgente */
+      // for (let i = 0; i < nodes.length; i += 1) {
+
+      /* solo eligo entre los nodos disponibles */
+      // if (nodes[i].blocked) continue;
+
+      // if (nodeA.analysis.timeProximity < nodeB.analysis.timeProximity)
+
+      // };
+
+      // console.log(nodeSelect);
+      /* fin de identificacion del nodo mas urgente */
+
     } while (false);
 
-    // eslint-disable-next-line no-console
+    console.log('nodes');
     console.log(nodes);
+    console.log('unfulfilled nodes');
+    console.log(unfulfilledNodes);
 
     return Promise.all(nodes);
 
--- src/services/brinks.service.js
+++ src/services/brinks.service.js
@@ -1,6 +1,5 @@
-/* eslint-disable quotes */
-const moment = require("moment");
-const fetch = require("node-fetch");
+const moment = require('moment');
+const fetch = require('node-fetch');
 
 const brinksService = {
   /* Funcion que obtiene la hora mas al final del dia de todas las franjas horarias de un nodo */
@@ -23,57 +22,58 @@ const brinksService = {
 
   /* description */
   getFirstStrip: async (node, currentTime) => {
-    const firstStrip = node.attentionHour.reduce(
-      (a, b, index, attentionHours) => {
-        const hourStartA = moment.duration(a.start);
-        const hourStartB = moment.duration(b.start);
+    const firstStrip = node.attentionHour.reduce((a, b, index, attentionHours) => {
+      const hourStartA = moment.duration(a.start);
+      const hourStartB = moment.duration(b.start);
 
-        if (hourStartA < hourStartB) return a;
-        // eslint-disable-next-line no-else-return
-        else if (hourStartB >= currentTime) return b;
+      if (hourStartA < hourStartB) return a;
+      // eslint-disable-next-line no-else-return
+      else if (hourStartB >= currentTime) return b;
 
-        const hourEndB = moment.duration(b.end);
-        /* si la franja venció entonces eliminamos dicha franja horaria */
-        if (hourEndB <= currentTime) attentionHours.splice(index, 1);
-        else return b;
+      const hourEndB = moment.duration(b.end);
+      /* si la franja venció entonces eliminamos dicha franja horaria */
+      if (hourEndB <= currentTime) attentionHours.splice(index, 1);
+      else return b;
 
-        return a; // si eliminamos la franja horaria en el if, devuelvo la franja A
+      return a; // si eliminamos la franja horaria en el if, devuelvo la franja A
+    });
 
-        // eslint-disable-next-line comma-dangle
-      }
-    );
+    /* haciendo una duracion start and end */
+    firstStrip.start = moment.duration(firstStrip.start);
+    firstStrip.end = moment.duration(firstStrip.end);
 
     return firstStrip;
   },
 
   /* description */
   getDurationInTraffic: async (nodeA, nodeB, currentDate, currentTime) => {
-    const baseUrl = "https://router.hereapi.com/v8/routes?";
+    const baseUrl = 'https://router.hereapi.com/v8/routes?';
     const apiKey = process.env.API_KEY_TRAFFIC;
 
     let time = moment(currentDate);
     time.add(currentTime);
-    time = time.format("YYYY-MM-DDTHH:mm:ss");
+    time = time.format('YYYY-MM-DDTHH:mm:ss');
 
-    const timeInTraffic = await fetch(
-      `${baseUrl}transportMode=car&origin=${nodeA.coordinates.lat},${nodeB.coordinates.lng}&destination=${nodeB.coordinates.lat},${nodeB.coordinates.lng}&return=summary&departureTime=${time}&apiKey=${apiKey}`
-    )
+    const timeInTraffic = await fetch(`${baseUrl}transportMode=car&origin=${nodeA.coordinates.lat},${nodeB.coordinates.lng}&destination=${nodeB.coordinates.lat},${nodeB.coordinates.lng}&return=summary&departureTime=${time}&apiKey=${apiKey}`)
       .then((res) => res.json())
       .then((res) => res.routes[0].sections[0].summary.duration);
 
-    return moment.duration(timeInTraffic, "seconds");
+    return moment.duration(timeInTraffic, 'seconds');
   },
 
   /* description */
-  getArrival: async (node, currentTime, durationInTraffic, strip) => {
-    const start = moment(strip.start, "HH:mm");
-    const end = moment(strip.end, "HH:mm");
+  getArrival: async (node, currentTime, durationInTraffic, firstStrip) => {
+    const start = moment('00:00:00', 'HH:mm:ss');
+    start.add(firstStrip.start);
 
-    const hourArrival = moment(`00:00:00`, "HH:mm:ss");
+    const end = moment('00:00:00', 'HH:mm:ss');
+    end.add(firstStrip.end);
+
+    const hourArrival = moment('00:00:00', 'HH:mm:ss');
     hourArrival.add(currentTime);
     hourArrival.add(durationInTraffic);
 
-    return moment(hourArrival).isBetween(start, end, null, "[]");
+    return hourArrival.isBetween(start, end, null, '[]');
   },
 
   /* description */
@@ -95,10 +95,9 @@ const brinksService = {
   checkExpiredTimeSlot: async (currentTime, durationInTraffic, firstStrip) => {
     const hourArrival = currentTime.clone();
     hourArrival.add(durationInTraffic);
-    const hourEnd = moment.duration(firstStrip.end);
 
     /* si la hora en la que llega es mayor a la hora final de la franja, la franja ya ha vencido */
-    return hourArrival > hourEnd;
+    return hourArrival > firstStrip.end;
   },
 };
 
